c786749335d34386aa3b3c4f22c995dba38c017a
Alex <ac5295@student.jamk.fi>
2024-02-11 23:10:49 +0200

    session provider

--- Client.React/src/App.tsx
+++ Client.React/src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import QueryProvider from "./context/query-provider/QueryProvider";
 import RouterProvider from "./context/router-provider/RouterProvider";
+import SessionProvider from "./context/session-provider/SessionProvider";
 
 function App() {
   return (
     <QueryProvider>
-      <RouterProvider />
+      <SessionProvider>
+        <RouterProvider />
+      </SessionProvider>
     </QueryProvider>
   );
 }
